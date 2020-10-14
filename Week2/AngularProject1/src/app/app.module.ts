b4759eb9f63dcf4e918b5486341e146ce2612051
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2020-10-14 14:59:22 +0530

    Added Components

--- Week2/AngularProject1/src/app/app.module.ts
+++ Week2/AngularProject1/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './LoginComponent/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
