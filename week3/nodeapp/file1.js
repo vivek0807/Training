45cb22b18563facf0dc029a4e1dbd4c690894d6b
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2020-10-21 12:51:58 +0530

    Added Week3

---
+++ week3/nodeapp/file1.js
@@ -0,0 +1,38 @@
+//var app=require('express');
+var http=require('http');
+const fs=require('fs')
+const data=fs.readFileSync('data.txt','utf-8');
+console.log(data)
+
+console.log("Message");
+fs.copyFile("data.txt", "data_copy.txt", (err) => { 
+    if (err) { 
+      console.log("Error Found:", err); 
+    } 
+    else { 
+      //getCurrentFilenames(); 
+      //console.log("\nFile Contents of copied_file:", 
+       // fs.readFileSync("copied_file.txt", "utf8")); 
+    } 
+  });
+
+fs.readFile('data.txt','utf-8',function(err,data){
+    if(err){
+        console.log("Error reading")
+    }
+    else
+        {http.createServer(function (req, res) {
+         //   res.writeHead(200, {'Content-Type': 'text/plain'});
+            if(req.url==="/")
+            {res.write("Hello World");
+            res.end();}
+            else if(req.url==="/great"){
+                res.write("<HTML><h2>Welcome</h2></HTML>");
+            res.end();
+            }
+          }).listen(55555)
+        
+        
+        }
+})
+
