1da20ab5fc105ba14746d7a75e2f82385ee8778f
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2020-11-23 21:44:37 +0530

    c#
    C3project Created

--- c#/codesBasics/codesBasics/Program.cs
+++ c#/codesBasics/codesBasics/Program.cs
@@ -3,10 +3,22 @@
 namespace codesBasics
 {
     class Program
+
     {
+        static void runself()
+        {
+            Console.WriteLine("running without call");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Give me a number");
+            int get = Convert.ToInt32(Console.ReadLine());
+            int n = 'a';
+            Console.WriteLine(get+"Fifty six");
+            runself();
+            Console.ReadKey();
+            
         }
     }
     
