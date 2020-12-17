3c9467b016f91f040461e29811061fe04403fc1c
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2020-12-17 22:32:40 +0530

    done

--- c#/CodeBasics/CodeBasics/Program.cs
+++ c#/CodeBasics/CodeBasics/Program.cs
@@ -15,7 +15,19 @@ namespace CodeBasics
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello ffrom N");
+            Console.WriteLine("fvcxcxvcx");
+        }
+    }
+
+    class main
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter two number");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            bool final = n1 > n2;
+            Console.WriteLine(final);
+       
         }
     }
 }
