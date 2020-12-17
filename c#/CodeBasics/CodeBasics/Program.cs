080fe938800e88f916009445798d0045ef53bac0
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2020-12-17 21:27:57 +0530

    fddg

---
+++ c#/CodeBasics/CodeBasics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeBasics
+{
+    class M
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+
+    class N
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ffrom N");
+        }
+    }
+}
