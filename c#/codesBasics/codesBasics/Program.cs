e77b7c83a63dac5adf95234edbe78905ea82357d
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2020-12-17 14:05:57 +0530

    c# App

--- c#/codesBasics/codesBasics/Program.cs
+++ c#/codesBasics/codesBasics/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-
 namespace codesBasics
 {
     class Program
-
-    {
-        static void runself()
+    { static string runself()
         {
             Console.WriteLine("running without call");
+            return "Called the method";
         }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Give me a number");
+
             int get = Convert.ToInt32(Console.ReadLine());
             int n = 'a';
             Console.WriteLine(get+"Fifty six");
